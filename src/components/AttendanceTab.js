b80ba96f572373e85b8dcb932a40c9cf13ba3c78
dotiennee <149573030+dotiennee@users.noreply.github.com>
2023-11-12 12:10:36 +0700

    Update AttendanceTab.js

--- src/components/AttendanceTab.js
+++ src/components/AttendanceTab.js
@@ -4,7 +4,13 @@ const AttendanceTab = () => {
     // Your attendance tab content
     return(
         <div class="row actcontent">
-            Tui mợt quá à
+            <div class="columcontent search">
+                Search ở đây nè
+            </div>
+            <div class="row row2">
+                <div class="columcontent col-6">Cột 1 </div>
+                <div class="columcontent col-6">Cột 2</div>
+            </div>
             
 
         </div>
