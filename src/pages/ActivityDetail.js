81fa719ca7a04e364613cc61b34fc4433539d38a
dotiennee <149573030+dotiennee@users.noreply.github.com>
2023-11-26 15:31:29 +0700

    oke rồi nè huhu

--- src/pages/ActivityDetail.js
+++ src/pages/ActivityDetail.js
@@ -15,10 +15,15 @@ import bell from '../image/Bell_fill.png';
 import user from '../image/user.png';
 import vector from '../image/vector.png';
 
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+
 function ActivityDetailPage() {
     let { id } = useParams();
 
-    const [role, setRole] = useState('sinhvien');
+    const [role, setRole] = useState('tovanphong');
 
     const [showConfirmation, setShowConfirmation] = useState(false);
 
@@ -29,120 +34,159 @@ function ActivityDetailPage() {
         // Add your registration logic here
     };
 
-    const renderButton = () => {
-        if (role === 'sinhvien') {
-            return (          
-                
-                    <div class="container-md">      
-                            <div class ="toprow row">
-                                <div class ="col">
-                                    <button class="btn btn-primary arrow-button-container">
-                                        <img class="arrow-icon" src={leftArrow} />
-                                    </button>
-                                    <button  class="btn btn-primary arrow-button-container">
-                                        <img class="arrow-icon" src={rightArrow} />
-                                    </button>
-                                </div>
-
-                                {/* <div class="col-3 notification-container"> */}
-                                <button class="btn btn-secondary arrow-button-container account-icon-container">
-                                    <img class="bell-icon" src={bell} alt="bell-icon"/>
-                                </button>
-
-                                <div class="user-account account-icon-container">
-                                    <img class="user-icon" src={user} />
-
-                                    <div class="user-infor">
-                                        <div class="user-name">Cinamon</div>
-                                        <div class="user-role">Admin</div>
-                                    </div>
-                                    <button class="btn btn-secondary vector-container">
-                                        <img  class="vector-icon" src={vector}/>
-                                    </button>
-                                </div>     
-                            </div>
-                            <div class="row acttitle">
-                                <div class="r2c1 col">
-                                    <h1>Tên hoạt động ở đây nè!!!</h1>
-                                </div>
-                                <div class="r2c2">
-                                    <div class="actnumber">4/8</div>
-                                    <button type="button" class="actresig btn" onClick={() => setShowConfirmation(true)}>Đăng ký</button>
-                                    {showConfirmation && <RegisterActivityConfirmationPopup onConfirm={handleConfirmRegistration} />}
-                                </div>
-                            </div>
-                            <TabComponent />
-                    </div> 
-                   
-               
-            );
-        } else if (role === 'admin') {
-            return <button>Chỉnh sửa</button>;
+    const getUserRoleName = (role) => {
+        switch (role) {
+          case 'admin':
+            return 'Admin';
+          case 'sinhvien':
+            return 'Sinh viên';
+          case 'tovanphong':
+            return 'Tổ văn phòng';
+          default:
+            return 'Khách';
         }
-        return null;
+      };
+
+    const renderButton = () => {        
+        return(
+            <div class="container-md" >      
+                    <div class ="toprow row">
+                        <div class ="col">
+                            <button class="btn btn-primary arrow-button-container">
+                                <img class="arrow-icon" src={leftArrow} />
+                            </button>
+                            <button  class="btn btn-primary arrow-button-container">
+                                <img class="arrow-icon" src={rightArrow} />
+                            </button>
+                        </div>
+
+                        {/* <div class="col-3 notification-container"> */}
+                        <Dropdown as={ButtonGroup} variant="user-account account-icon-container">
+                            <Button variant="user-account account-icon-container">
+                            <div class="user-account account-icon-container">
+                                <img class="user-icon" src={user} />
+
+                                <div class="user-infor">
+                                    <div class="user-name">Cinamon</div>
+                                    <div class="user-role">{getUserRoleName(role)}</div>
+                                </div>
+                            </div>
+                            </Button>
+
+                            <Dropdown.Toggle split variant="user-account account-icon-container" id="drop-split-basic" />
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#">Cài đặt tài khoản</Dropdown.Item>
+                                <Dropdown.Item href="#">Chỉnh sửa thông tin</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown> 
+                    </div>
+                    <div class="row acttitle">
+                        <div class="r2c1 col">
+                            <h1>Tên hoạt động ở đây nè!!!</h1>
+                        </div>
+                        <div class="r2c2">
+                            <div class="actnumber">4/8</div>
+                            <SelectButton />
+                        </div>
+                    </div>
+                    <TabComponent />
+            </div>     
+        );       
     };
 
+    const TabComponent = () => {
+        const [currentTab, setCurrentTab] = useState('activityContent');
+    
+        const renderTabContent = (tab) => {
+            switch (tab) {
+                case 'activityContent':
+                    return <ActivityContentTab />;
+                case 'studentList':
+                    return <StudentListTab />;
+                case 'activityForum':
+                    return <ActivityForumTab />;
+                case 'attendance':
+                    return <AttendanceTab />;
+                default:
+                    return null;
+            }
+        };
+    
+        return (
+            <div class="actlayout">
+                <div class="row actbutton">
+                    <button  
+                        type="button" 
+                        // class="detailbutton btn btn-outline-primary" 
+                        className={`detailbutton btn  ${currentTab === 'activityContent' ? 'active' : 'btn-outline-primary'}`}
+                        onClick={() => setCurrentTab('activityContent')}>Nội dung hoạt động
+                    </button>
+                    <button  
+                        type="button" 
+                        className={`detailbutton btn  ${currentTab === 'studentList' ? 'active' : 'btn-outline-primary'}`}
+                        onClick={() => setCurrentTab('studentList')}>Danh sách sinh viên
+                    </button>
+                    <button  
+                        type="button" 
+                        className={`detailbutton btn ${currentTab === 'activityForum' ? 'active' : ' btn-outline-primary'}`}
+                        onClick={() => setCurrentTab('activityForum')}>Thông báo hoạt động
+                    </button>
+                    {role==='admin' &&(
+                    <button  
+                        type="button" 
+                        className={`detailbutton btn  ${currentTab === 'attendance' ? 'active' : 'btn-outline-primary'}`}
+                        onClick={() => setCurrentTab('attendance')}>Điểm danh
+                    </button>
+                    )}
+                </div>
+                
+                {renderTabContent(currentTab)}
+                
+                
+            </div>
+        );
+    };
+
+    const SelectButton = () => {
+        if (role === 'sinhvien') {
+            return(
+                <>
+                    <button type="button" class="actresig-sinhvien btn" onClick={() => setShowConfirmation(true)}>Tham gia</button>
+                    {showConfirmation && <RegisterActivityConfirmationPopup onConfirm={handleConfirmRegistration} />}
+                </>
+            );
+        }else if (role === 'admin') {
+            return(
+                <>
+                    <button type="button" class="actresig-admin btn" onClick={() => setShowConfirmation(true)}>Cập nhật</button>
+                    {showConfirmation && <RegisterActivityConfirmationPopup onConfirm={handleConfirmRegistration} />}
+                </>
+            );
+        }else{
+            return (
+                <>
+                <button type="button" class="actresig-admin btn" onClick={() => setShowConfirmation(true)}>Duyệt</button>
+                {showConfirmation && <RegisterActivityConfirmationPopup onConfirm={handleConfirmRegistration} />}
+                <button type="button" class="actresig-tvp btn" onClick={() => setShowConfirmation(true)}>Xóa</button>
+                    {showConfirmation && <RegisterActivityConfirmationPopup onConfirm={handleConfirmRegistration} />}
+                </>
+            );
+        } 
+
+       
+    }
+
     return (
-        <div>
+        <>
             {/* <h1>Chi tiết hoạt động {id}</h1> */}
             {renderButton()}
             {/* <TabComponent /> */}
-        </div>
+        </>
     );
 }
 
 // Tab component
-const TabComponent = () => {
-    const [currentTab, setCurrentTab] = useState('activityContent');
 
-    const renderTabContent = (tab) => {
-        switch (tab) {
-            case 'activityContent':
-                return <ActivityContentTab />;
-            case 'studentList':
-                return <StudentListTab />;
-            case 'activityForum':
-                return <ActivityForumTab />;
-            case 'attendance':
-                return <AttendanceTab />;
-            default:
-                return null;
-        }
-    };
-
-    return (
-        <div class="actlayout">
-            <div class="row actbutton">
-                <button  
-                    type="button" 
-                    // class="detailbutton btn btn-outline-primary" 
-                    className={`detailbutton btn  ${currentTab === 'activityContent' ? 'active' : 'btn-outline-primary'}`}
-                    onClick={() => setCurrentTab('activityContent')}>Nội dung hoạt động
-                </button>
-                <button  
-                    type="button" 
-                    className={`detailbutton btn  ${currentTab === 'studentList' ? 'active' : 'btn-outline-primary'}`}
-                    onClick={() => setCurrentTab('studentList')}>Danh sách sinh viên
-                </button>
-                <button  
-                    type="button" 
-                    className={`detailbutton btn ${currentTab === 'activityForum' ? 'active' : ' btn-outline-primary'}`}
-                    onClick={() => setCurrentTab('activityForum')}>Thông báo hoạt động
-                </button>
-                {role==='admin' &&(
-                <button  
-                    type="button" 
-                    className={`detailbutton btn  ${currentTab === 'attendance' ? 'active' : 'btn-outline-primary'}`}
-                    onClick={() => setCurrentTab('attendance')}>Điểm danh
-                </button>
-                )}
-            </div>
-            
-            {renderTabContent(currentTab)}
-            
-            
-        </div>
-    );
-};
 
 export default ActivityDetailPage;
