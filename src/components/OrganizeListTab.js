da9b7efb21ca4c6c98752f4ce80cb80f89ae4bf1
meomeo253 <150012008+meomeo253@users.noreply.github.com>
2024-01-01 12:39:13 +0700

    Update OrganizeListTab.js

--- src/components/OrganizeListTab.js
+++ src/components/OrganizeListTab.js
@@ -4,22 +4,19 @@ import '../style/UserHistory.css';
 
 function OrganizeListTab() {
     // Giả sử bạn có một mảng chứa thông tin sinh viên
-    const students = [
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' },
-        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', status: 'Đăng ký thành công', exresult: '2 ngày CTXH', result: 'Chưa có' }
-        // ...Thêm thông tin sinh viên khác tại đây
+    const organizers = [
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'TrucThanhnef', quantity: '5/250', status: 'Sắp diễn ra' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'DoTiennef', quantity: '8/10', status: 'Đang diễn ra' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'ThuyTiennef', quantity: '10/10', status: 'Đã kết thúc' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'MinhThuannef', quantity: '10/10', status: 'Đã kết thúc' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'BaoNgocnef', quantity: '10/10', status: 'Đã kết thúc' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'NhatHanef', quantity: '10/10', status: 'Đã hủy' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'TrucThanhnef', quantity: '10/10', status: 'Đã hủy' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'DoTiennef', quantity: '10/10', status: 'Đã hủy' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'ThuyTiennef', quantity: '10/10', status: 'Đã hủy' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'MinhThuannef', quantity: '10/10', status: 'Đã hủy' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'BaoNgocnef', quantity: '10/10', status: 'Đã hủy' },
+        { id: 1, name: 'Trực phát thiết bị...', time: '10:28:00 - 7 Th11 2023', oname: 'NhatHanef', quantity: '10/10', status: 'Đã hủy' }
     ];
 
     return (
@@ -30,22 +27,22 @@ function OrganizeListTab() {
                         <tr class="titlerow">
                             <th scope="col-1" className="sticky-header">Mã số</th>
                             <th scope="col-3" className="sticky-header">Tên hoạt động</th>
-                            <th scope="col-2" className="sticky-header">Thời gian đăng ký</th>
-                            <th scope="col-3" className="sticky-header">Trạng thái</th>
-                            <th scope="col-3" className="sticky-header">Kết quả dự kiến</th>
-                            <th scope="col-2" className="sticky-header">Kết quả</th>
+                            <th scope="col-2" className="sticky-header">Thời gian tạo</th>
+                            <th scope="col-3" className="sticky-header">Đơn vị tạo</th>
+                            <th scope="col-2" className="sticky-header">Số lượng</th>
+                            <th scope="col-3" className="sticky-header">Tình trạng</th>
                         </tr>
                     </thead>  
 
                     <tbody>
-                        {students.map((student, index) => (
+                        {organizers.map((organizer, index) => (
                         <tr>
-                            <td>{student.id}</td>
-                            <td>{student.name}</td>
-                            <td>{student.time}</td>
-                            <td>{student.status}</td>
-                            <td>{student.exresult}</td>
-                            <td>{student.result}</td>
+                            <td>{organizer.id}</td>
+                            <td>{organizer.name}</td>
+                            <td>{organizer.time}</td>
+                            <td>{organizer.oname}</td>
+                            <td>{organizer.quantity}</td>
+                            <td>{organizer.status}</td>
                         </tr>
                         ))}
                     </tbody> 
